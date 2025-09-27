31ad3a034c158008ad6f59a9bfb4a54c126d59ec
dungpham108 <dungpham1008.dev@gmail.com>
2025-09-27 11:57:51 +0700

    fix server

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://to-do-list-smoky-delta-51.vercel.app", // domain frontend của bạn
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
